a47a1c1630c25e170c6c1898ace6dbcbecc4bc14
温昌寿 <wenchangshou@live.cn>
2018-04-02 18:16:20 +0800

    初步状态，完成react+electron+redux实现一个TODO

---
+++ src/containers/FilterLink.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import { setVisibilityFilter } from '../actions';
+import Link from '../components/Link';
+
+const mapStateToProps = (state, ownProps) => ({
+  active: ownProps.filter === state.setVisibilityFilter,
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick: () => {
+    dispatch(setVisibilityFilter(ownProps.filter));
+  },
+});
+
+const FilterLink = connect(mapStateToProps, mapDispatchToProps)(Link);
+export default FilterLink;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(todoApp);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+registerServiceWorker();
